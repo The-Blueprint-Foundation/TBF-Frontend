13c5a4483f79c5feccb4cb6f7907dd8773f84b4b
Lex Albrandt <lex.albrandt@gmail.com>
2026-07-28 15:48:55 -0700

    Task: Add global colorsheet link in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import "./index.css";
+import "../css/colors.css";
 import App from "./App.jsx";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-	iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+	iconRetinaUrl:
+		"https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
 	iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
 	shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
